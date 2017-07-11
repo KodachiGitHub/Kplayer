3dbeef5b17eb2de80bf53af9e4ee9c84ea37b074
Kodachi <1605635009@qq.com>
2017-07-12 00:38:27 +0800

    bug fix

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueResource from 'vue-resource'
-import VueMaterial  from 'vue-material'
 import Api from './ApiService'
 import store from './store/index'
 
 Vue.use(VueResource);
-Vue.use(VueMaterial);
 
 Vue.prototype.$api = Api;
 
